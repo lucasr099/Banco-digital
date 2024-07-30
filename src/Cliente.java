2543c93e4baa1cd9acc456b417c2b31b74c07983
lucasr099 <141447167+lucasr099@users.noreply.github.com>
2024-07-30 13:18:15 -0300

    Add files via upload

---
+++ src/Cliente.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Cliente {
+    private String nome;
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+}
+
